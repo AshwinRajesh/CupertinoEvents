318295c4c6472c1517402d303a0f1767a544d672
AshwinRajesh <ashwinr2003@yahoo.com>
2020-07-24 14:30:18 -0700

    Added map page and functionality

---
+++ map.js
@@ -0,0 +1,74 @@
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+var map;
+
+function initMap() {
+	map = new google.maps.Map(
+	  document.getElementById('map'), {
+	  	center: {
+            lat: 37.3230,
+            lng: -122.0322
+        },
+        zoom: 14
+	  }
+	);
+	addMarker({lat: 37.3230, lng: -122.0322}, "Sample Event", "Come join us at this sample event!", new Date("July 24 2020 12:00:00"), new Date("July 24 2020 15:00:00"));
+}
+
+function formatDate(date) {
+	var hour = (date.getHours() % 12 == 0) ? 12 : date.getHours() % 12;
+	var ampm = (date.getHours() > 11) ? "pm" : "am";
+	var minute = (date.getMinutes() < 10) ? ("0" + date.getMinutes()) : date.getMinutes()
+	var full = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() + ", " + hour + ":" + minute + ampm;
+	return full;
+}
+
+function addMarker(position, title, description, start, end) {
+	var marker = new google.maps.Marker({
+	  	position: position, 
+	  	map: map
+  	});
+
+  	/*var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h4 id="firstHeading" class="firstHeading" style="margin-bottom: 15px">' + title + '</h4>'+
+      '<div id="bodyContent">'+
+      '<p>' + description +
+      '<p>'+ month + ' ' + day + ', ' + year + '</p>'+
+      '<p>'+ start + ' to ' + end + '</p>'+
+      '</div>'+
+      '</div>';*/
+  	marker.addListener('click', function() {
+    	var today = new Date();
+    	var contentString = "";
+    	if (start < today && today < end) {
+    		contentString = '<div id="content">'+
+		      '<div id="siteNotice">'+
+		      '</div>'+
+		      '<h4 id="firstHeading" class="firstHeading" style="margin-bottom: 15px">' + title + '</h4>'+
+		      '<div id="bodyContent">'+
+		      '<p>' + description +
+		      '<p>' + 'Ongoing until ' + formatDate(end) + '</p>'+
+		      '</div>'+
+		      '</div>';
+    	} else {
+    		contentString = '<div id="content">'+
+		      '<div id="siteNotice">'+
+		      '</div>'+
+		      '<h4 id="firstHeading" class="firstHeading" style="margin-bottom: 15px">' + title + '</h4>'+
+		      '<div id="bodyContent">'+
+		      '<p>' + description +
+		      '<p>' + formatDate(start) + ' to ' + formatDate(end) + '</p>'+
+		      '</div>'+
+		      '</div>';
+    	}
+    	var infoWindow = new google.maps.InfoWindow({
+	  		content: contentString
+	  	});
+    	infoWindow.open(map, marker);
+		
+  	});
+}
